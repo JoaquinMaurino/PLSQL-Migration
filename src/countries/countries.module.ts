811847361e5eff8c2094227efdc723e96bc9ba11
Joaquin <joaquin9918@gmail.com>
2025-05-27 16:30:18 -0300

    controller y service de countries

--- src/countries/countries.module.ts
+++ src/countries/countries.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { CountriesController } from './controller/countries/countries.controller';
-import { CountriesService } from './service/countries/countries.service';
+import { CountriesController } from './controller/countries.controller';
+import { CountriesService } from './service/countries.service';
 
 @Module({
   controllers: [CountriesController],
